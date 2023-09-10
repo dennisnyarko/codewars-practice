d2c2c96589c09fe7e95ccad891571ff3433dcce7
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-09-10 23:06:57 +0000

    Update whats-in-a-name.py

--- whats-in-a-name.py
+++ whats-in-a-name.py
@@ -5,3 +5,33 @@
 
 
 #solution
+
+def name_in_str(str, name):
+    pass # your code here
+    # Convert both strings to lowercase for case-insensitive comparison
+    str = str.lower()
+    name = name.lower()
+
+    # Initialize pointers for both strings
+    i = 0  # Pointer for str1
+    j = 0  # Pointer for str2
+
+    # Iterate through str1
+    while i < len(str):
+        # If the current character in str1 matches the current character in str2,
+        # move the pointer for str2 to the next character.
+        if str[i] == name[j]:
+            j += 1
+            # If we have matched all characters in str2, return True
+            if j == len(name):
+                return True
+        i += 1
+
+    # If we reach the end of str1 and haven't matched all characters in str2, return False
+    return False
+
+# Example usage:
+str = "Hello World"
+name = "loW"
+result = name_in_str(str, name)
+print(result)  # Output: True
