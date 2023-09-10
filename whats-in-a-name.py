e33351a66697fd1061b96446fdae60e4ebbc0516
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-09-10 23:04:49 +0000

    Create whats-in-a-name.py

---
+++ whats-in-a-name.py
@@ -0,0 +1,4 @@
+#Task
+
+#Write a function, taking two strings in parameter, that tests whether or not the first string contains all of the letters of the second string, in order.
+#The function should return true if that is the case, and else false. Letter comparison should be case-INsensitive.
