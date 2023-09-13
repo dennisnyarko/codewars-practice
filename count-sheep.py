6a033e5a36c7a087f6f9a10fba4b05e89b5bfed6
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-09-13 22:14:31 +0000

    Update count-sheep.py

--- count-sheep.py
+++ count-sheep.py
@@ -1,1 +1,9 @@
 #Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+#solution
+def count_sheep(n):
+    # your code
+    murmur = ""
+    for i in range(1, n + 1):
+        murmur += f"{i} sheep..."
+    return murmur
