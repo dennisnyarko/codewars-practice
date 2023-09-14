6c45e26878dc54cfa2296969638252a4a65e16c6
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-09-14 23:24:58 +0000

    Update twice-as-old.py

--- twice-as-old.py
+++ twice-as-old.py
@@ -6,3 +6,11 @@
     #current age of his son (years)
 
 #Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+
+#Solution
+
+def twice_as_old(dad_age, son_age):
+    # Calculate how many years ago the father was twice as old as his son
+    years_ago = abs(dad_age - 2 * son_age)
+    return years_ago
