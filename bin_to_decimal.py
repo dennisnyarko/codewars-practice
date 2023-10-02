42fe476f31d556d395c7401b7679268e674838a3
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-10-02 23:54:01 +0000

    Update bin_to_decimal.py

--- bin_to_decimal.py
+++ bin_to_decimal.py
@@ -2,3 +2,12 @@
 Question
 Complete the function which converts a binary number (given as a string) to a decimal number.
 """
+
+#solution
+
+def bin_to_decimal(inp):
+    try:
+        decimal_num = int(inp, 2)
+        return decimal_num
+    except ValueError:
+        return "Invalid binary input"
