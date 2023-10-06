1ce8dba5892ee87de199d11c4b2308d6578b8709
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-10-06 14:19:18 +0000

    Update array_plus_array.py

--- array_plus_array.py
+++ array_plus_array.py
@@ -1,1 +1,10 @@
+"""
+Question
+Get the sum of two arrays... Actually the sum of all their elements
+P.S. Each array includes only integer numbers. Output is a number too.
 
+"""
+
+#Solution
+def array_plus_array(arr1,arr2):
+    return sum(arr1 + arr2)
