5d3e30a8f8a0ab4f766f5229e542e61c71624310
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-10-18 19:55:52 +0000

    Update return_string.py

--- return_string.py
+++ return_string.py
@@ -1,3 +1,6 @@
 #Question
 #Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
 
+#Solution
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
