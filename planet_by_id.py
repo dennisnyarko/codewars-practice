b015b92c9efc2e6afc10caacff93f90ae3056dce
Dennis Nyarko <dennis_nyarko@outlook.com>
2023-10-18 20:21:59 +0000

    Update planet_by_id.py

--- planet_by_id.py
+++ planet_by_id.py
@@ -18,3 +18,17 @@ def get_planet_name(id):
         case 8: name = "Neptune"
     return name
 """    
+
+#Solution
+def get_planet_name(id):
+    planet_names = {
+        1: "Mercury",
+        2: "Venus",
+        3: "Earth",
+        4: "Mars",
+        5: "Jupiter",
+        6: "Saturn",
+        7: "Uranus" , 
+        8: "Neptune"
+        }
+    return planet_names.get(id, "Unknown")
